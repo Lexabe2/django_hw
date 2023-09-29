750dc840338c146ae31e687813c175bb07455429
Alekseu <natash79@inbox.ru>
2023-09-29 08:06:42 +0300

    update

--- mysite/hw/admin.py
+++ mysite/hw/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Order, Client, Product
 
-# Register your models here.
+admin.site.register(Order)
+admin.site.register(Client)
+admin.site.register(Product)
--- mysite/hw/views.py
+++ mysite/hw/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 import logging
+from datetime import datetime, timedelta
+from .models import Order
 
 logger = logging.getLogger(__name__)
 
 name = 'User Alex'
 
+
 def index(request):
-    logger.info(f"{name} I went to the main page")
-    return render(request, 'hw/index.html')
+    now = datetime.now()
+
+    orders_7_days = Order.objects.filter(date__gte=now - timedelta(days=7))
+
+    orders_30_days = Order.objects.filter(date__gte=now - timedelta(days=30))
+
+    orders_365_days = Order.objects.filter(date__gte=now - timedelta(days=365))
+
+    return render(request, 'hw/index.html', {
+        'orders_7_days': orders_7_days,
+        'orders_30_days': orders_30_days,
+        'orders_365_days': orders_365_days,
+    })
+
 
 def about_me(request):
     logger.info(f"{name} Went to the main page about me")
